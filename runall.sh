7f9b42f3f8d9d9a12c82a4292668afa311502c3f
Chris Kanich <ckanich@uic.edu>
2020-03-02 08:25:34 -0600

    works without wget

--- runall.sh
+++ runall.sh
@@ -5,7 +5,7 @@ echo starting 6 way measurement at `date`
 # get the alexa for today
 TODAY=$(date '+%Y%m%d')
 mkdir -p output
-wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
+curl http://s3.amazonaws.com/alexa-static/top-1m.csv.zip > top-1m.csv.zip
 unzip -c top-1m.csv.zip top-1m.csv | head -n 1000 | awk -F, '{print "http://" $2}' > output/$TODAY.top1k.txt 
 
 for profilenum in 1 2 ; do
