bd91a77d255d5d047b6a41a47eaba966c1b6e31c
Panagiotis Papadopoulos <panpap@gmx.com>
2020-05-07 13:31:55 +0100

    annotations fix

--- privacykpis/stats.py
+++ privacykpis/stats.py
@@ -1,9 +1,10 @@
 import argparse
 import pickle
-from typing import Optional
-from networkx import MultiDiGraph
-from networkx import read_gpickle
+import networkx  # type: ignore
+from networkx import MultiDiGraph, read_gpickle
 import json
+from typing import TextIO, Dict, List, Optional, Tuple, Any
+
 import privacykpis.args
 
 
@@ -15,34 +16,39 @@ class Args(privacykpis.args.Args):
         self.is_valid = True
 
 
-def requested_sites_subgraphs(graph: MultiDiGraph):
-    pass
+KeyPairsOrigins = Dict[Tuple[str, str], List[Any]]
 
-def get_origins(input_graph: MultiDiGraph):
-    initOrigins=[]
+# def requested_sites_subgraphs(graph: MultiDiGraph):
+#    pass
+
+
+def get_origins(input_graph: MultiDiGraph) -> List[str]:
+    initOrigins = []
     for n, d in list(input_graph.nodes(data=True)):
         # not sure why but there is None site in the trace
-        if n == None or d == None:
+        if n is None or d is None:
             print("Something wrong happened? Node: "+str(n)+" "+str(d))
             continue
         if d["type"] == "site":
             initOrigins.append(n)
     return initOrigins
 
-def compare_pairs(fw,prev_qtokens:dict,tp:str) -> dict:
+
+def compare_pairs(fw: TextIO, prev_qtokens: KeyPairsOrigins,
+                  tp: str) -> List[Dict[str, Any]]:
     keyvals = []
     for pp in prev_qtokens:
-        if len(prev_qtokens[pp])>1:
+        if len(prev_qtokens[pp]) > 1:
             for arr in prev_qtokens[pp]:
-                k,v=pp
-                fw.write(tp+"\t"+k+"\t"+v+"\t"+arr[0]+"\t"+str(arr[1])+"\t"+arr[2]+"\n")
-                keyvals.append({"key":k,"val":v,"origin":arr[0],"cookies":arr[1],"timestamp":arr[2]})
-    if len(keyvals)>0:
-        return keyvals
-    else:
-        return None
+                k, v = pp
+                fw.write(tp+"\t"+k+"\t"+v+"\t"+arr[0]+"\t"+str(arr[1]) +
+                            "\t"+arr[2]+"\n")
+                keyvals.append({"key": k, "val": v, "origin": arr[0],
+                                "cookies": arr[1], "timestamp": arr[2]})
+    return keyvals
 
-def measure_samekey_difforigin(args: Args):
+
+def measure_samekey_difforigin(args: Args) -> None:
     filename = args.input.name.split(".")[0]
     input_graph: MultiDiGraph = read_gpickle(args.input.name)
     control_graph: Optional[MultiDiGraph] = None
@@ -50,30 +56,30 @@ def measure_samekey_difforigin(args: Args):
     if args.control:
         control_graph = read_gpickle(args.control)
     origins = get_origins(input_graph)
-    fw=open(filename+"_results.tsv","w")
+    fw = open(filename+"_results.tsv", "w")
     fw.write("third party\tkey\tvalue\torigin\tcookies\ttimestamp\n")
     for n, d in list(input_graph.nodes(data=True)):
         # get 3party only
-        if  n == None or d["type"] == "site":
-            continue     
-        params_from_all_origins={}
+        if n is None or d["type"] == "site":
+            continue
+        params_all_origins: KeyPairsOrigins = {}
         for o in input_graph.predecessors(n):
             # check edge with origin
             if o in origins and (o, n) in input_graph.edges:
                 reqs = input_graph.get_edge_data(o, n)
                 for i in reqs:
-                    if reqs[i]["query tokens"] == None:
+                    if reqs[i]["query tokens"] is None:
                         continue
-                    #get qtokens for all origins associated with this 3party
+                    # get qtokens for all origins associated with this 3party
                     for pp in reqs[i]["query tokens"]:
                         cookies = reqs[i]["cookies tokens"]
                         timestamp = reqs[i]["timestamp"]
-                        if pp not in params_from_all_origins:
-                            params_from_all_origins[pp]=[]
-                        params_from_all_origins[pp].append([o,cookies,timestamp])
-        temp = compare_pairs(fw,params_from_all_origins,n)
-        if temp != None:
+                        if pp not in params_all_origins:
+                            params_all_origins[pp] = []
+                        params_all_origins[pp].append([o, cookies, timestamp])
+        temp = compare_pairs(fw, params_all_origins, n)
+        if len(temp) > 0:
             results[n] = temp
     fw.close
-    with open(filename+"_results.json","w") as fw:
+    with open(filename+"_results.json", "w") as fw:
         fw.write(json.dumps(results))
