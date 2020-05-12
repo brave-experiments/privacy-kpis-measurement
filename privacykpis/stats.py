744102c7e9028df99b9b911034f67dc6f5bd4494
Panagiotis Papadopoulos <panpap@gmx.com>
2020-05-12 21:08:09 +0100

    [produce detailed report per 3rd party

--- privacykpis/stats.py
+++ privacykpis/stats.py
@@ -16,7 +16,7 @@ class Args(privacykpis.args.Args):
         self.is_valid = True
 
 
-KeyPairsOrigins = Dict[Tuple[str, str], List[Any]]
+KeyPairsOrigins = Dict[str, List[Dict[str, Any]]]
 
 # def requested_sites_subgraphs(graph: MultiDiGraph):
 #    pass
@@ -34,18 +34,40 @@ def get_origins(input_graph: MultiDiGraph) -> List[str]:
     return initOrigins
 
 
-def compare_pairs(fw: TextIO, prev_qtokens: KeyPairsOrigins,
-                  tp: str) -> List[Dict[str, Any]]:
+def compare_and_print(fw: TextIO, prev_qtokens: KeyPairsOrigins,
+                      tp: str) -> List[Dict[str, Any]]:
     keyvals = []
-    for pp in prev_qtokens:
-        if len(prev_qtokens[pp]) > 1:
-            for arr in prev_qtokens[pp]:
-                k, v = pp
-                fw.write(tp+"\t"+k+"\t"+v+"\t"+arr[0]+"\t"+str(arr[1]) +
-                            "\t"+arr[2]+"\n")
-                keyvals.append({"key": k, "val": v, "origin": arr[0],
-                                "cookies": arr[1], "timestamp": arr[2]})
+    for k in prev_qtokens:
+        if len(prev_qtokens[k]) > 1:
+            for obj in prev_qtokens[k]:
+                fw.write(tp+"\t"+k+"\t"+obj["val"]+"\t"+obj["origin"] +
+                         "\t"+obj["type"]+"\t"+obj["timestamp"]+"\n")
+                keyvals.append({"key": k, "val": obj["val"],
+                                "origin": obj["origin"], "type": obj["type"],
+                                "timestamp": obj["timestamp"]})
     return keyvals
+
+
+def get_keypairs(o: str, req: Dict[str, Any],
+                 kpairs: KeyPairsOrigins) -> KeyPairsOrigins:
+    kpairs = demul_token(o, req, "query tokens", kpairs)
+    kpairs = demul_token(o, req, "cookies tokens", kpairs)
+    kpairs = demul_token(o, req, "path tokens", kpairs)
+    kpairs = demul_token(o, req, "body tokens", kpairs)
+    return kpairs
+
+
+def demul_token(o: str, req: Dict[str, Any], token_type: str,
+                kpairs: KeyPairsOrigins) -> KeyPairsOrigins:
+    tokens = req[token_type]
+    if tokens is None:
+        return kpairs
+    for (k, v) in tokens:
+        if k not in kpairs:
+            kpairs[k] = []
+        kpairs[k].append({"origin": o, "val": v, "type": token_type,
+                         "timestamp": req["timestamp"]})
+    return kpairs
 
 
 def measure_samekey_difforigin(args: Args) -> None:
@@ -57,27 +79,20 @@ def measure_samekey_difforigin(args: Args) -> None:
         control_graph = read_gpickle(args.control)
     origins = get_origins(input_graph)
     fw = open(filename+"_results.tsv", "w")
-    fw.write("third party\tkey\tvalue\torigin\tcookies\ttimestamp\n")
+    fw.write("third party\tkey\tvalue\torigin\ttype\ttimestamp\n")
     for n, d in list(input_graph.nodes(data=True)):
         # get 3party only
         if n is None or d["type"] == "site":
             continue
-        params_all_origins: KeyPairsOrigins = {}
+        keypairs_orgs: KeyPairsOrigins = {}
         for o in input_graph.predecessors(n):
             # check edge with origin
             if o in origins and (o, n) in input_graph.edges:
                 reqs = input_graph.get_edge_data(o, n)
                 for i in reqs:
-                    if reqs[i]["query tokens"] is None:
-                        continue
-                    # get qtokens for all origins associated with this 3party
-                    for pp in reqs[i]["query tokens"]:
-                        cookies = reqs[i]["cookies tokens"]
-                        timestamp = reqs[i]["timestamp"]
-                        if pp not in params_all_origins:
-                            params_all_origins[pp] = []
-                        params_all_origins[pp].append([o, cookies, timestamp])
-        temp = compare_pairs(fw, params_all_origins, n)
+                    # get tokens for all origins associated with this 3party
+                    keypairs_orgs = get_keypairs(o, reqs[i], keypairs_orgs)
+        temp = compare_and_print(fw, keypairs_orgs, n)
         if len(temp) > 0:
             results[n] = temp
     fw.close
