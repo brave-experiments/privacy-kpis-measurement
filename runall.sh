b59fae03249a7c2ada07bc5221d7a7bdaf3741a0
Chris Kanich <ckanich@uic.edu>
2020-07-15 20:11:54 -0500

    edits that were in use as of june 24th

--- runall.sh
+++ runall.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo starting 8 way measurement at `date`
-
 for profilenum in 1 2 ; do
     for browser in chrome brave firefox chrome-ubo ; do
             docker run -d -v privacykpis.profiles.$profilenum:/tmp/profiles --privileged kaytwo/privacykpis:$browser --profile-index $profilenum
     done
 done 
-
-echo finished 8 way measurement at `date`
--- rundaily.sh
+++ rundaily.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+export HOME=/home/ckanich
 
 cd /home/ckanich/repos/privacy-kpis-measurement
+./runall.sh > todays_dockers
 python3 source.py
 python3 sink.py
+gzip *.json && aws s3 cp *.json.gz s3://com.brave.research.privacykpis/ && rm *.json.gz
+docker stop $(cat todays_dockers)
+docker rm $(cat todays_dockers)
+rm todays_dockers
