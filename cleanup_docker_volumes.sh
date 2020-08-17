cfe36818fdbe09463ca3f40504448ad2190cd226
Chris Kanich <ckanich@uic.edu>
2020-08-17 14:10:46 -0500

    some combat fixes

--- cleanup_docker_volumes.sh
+++ cleanup_docker_volumes.sh
@@ -3,5 +3,5 @@
 # run this if you want to reset the profile storage within the internal docker volume storage. This
 # is the default location that runall.sh will store them.
 
-docker volume rm privacykpis.profiles.1
-docker volume rm privacykpis.profiles.2
+docker volume rm privacykpis.profiles.3
+docker volume rm privacykpis.profiles.4
