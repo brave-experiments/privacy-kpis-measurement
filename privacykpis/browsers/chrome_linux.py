c3e9fa7402a02bcc791905d01bf57a99e533ba0d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 00:22:24 +0000

    Adding tarfile member sanitization to extractall()

--- privacykpis/browsers/chrome_linux.py
+++ privacykpis/browsers/chrome_linux.py
@@ -114,7 +114,26 @@ class Browser(privacykpis.browsers.Interface):
                 profile_template = RESOURCES_PATH / args.profile_template
                 possible_profile_path.mkdir(parents=True)
                 with tarfile.open(profile_template) as tf:
-                    tf.extractall(args.profile_path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tf, args.profile_path)
 
         _edit_profile_to_prevent_session_restore(args.profile_path)
 
